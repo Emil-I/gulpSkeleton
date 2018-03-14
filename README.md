c7e376a569018780be4d1804fb878b1815466c60
Emil <emill.zuball@gmail.com>
2018-03-14 21:27:15 +0300

    add

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # gulpSkeleton
-Start app
+### Installation
+What you need:
+1. [NPM](https://www.npmjs.com/)
+2. [Gulp v4](http://gulpjs.com/)
+
+#### Then install all dependencies
+```bash
+npm install
+```
+
+<!-- Then make `gulp move` task
+and finishly you can start `gulp` -->
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
 'use strict';
 
 const gulp = require('gulp');
+const del = require('del');
+// const sourcemaps = require('gulp-sourcemaps');
 
+
+gulp.task('clean', function() {
+  return del('app/dist');
+});
 
 gulp.task('default', function() {
   return gulp.src('app/src/*.html')
     .pipe(gulp.dest('dist'));
-})
+});
+
+
+gulp.task('copy:image', function() {
+  return gulp.src('app/src/img/**', {
+      base: 'app/src'
+    })
+    .pipe(gulp.dest('app/dist/'))
+});
+
+
+gulp.task('build', gulp.series('clean', gulp.parallel('default', 'copy:image')));
